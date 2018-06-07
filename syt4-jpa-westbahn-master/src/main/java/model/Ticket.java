c1a3527304dfa1c19e6a4760abed3d7b4a0ecee7
afreudenthaler-tgm <afreudenthaler@student.tgm.ac.at>
2018-06-07 16:24:00 +0200

    Add files via upload
    Ticket Entity

--- syt4-jpa-westbahn-master/src/main/java/model/Ticket.java
+++ syt4-jpa-westbahn-master/src/main/java/model/Ticket.java
@@ -1,10 +1,23 @@
 package model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public abstract class Ticket {
 
+	@Id
+	@GeneratedValue
 	protected Long ID;
 
 	protected Strecke strecke;
 
 	protected Zahlung zahlung;
 
+	public Ticket(Strecke strecke, Zahlung zahlung) {
+		this.strecke = strecke;
+		this.zahlung = zahlung;
+	}
 }
