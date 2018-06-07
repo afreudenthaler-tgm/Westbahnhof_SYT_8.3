158f94f8947ef8f2bdb4cf19591a647d393662fb
afreudenthaler-tgm <afreudenthaler@student.tgm.ac.at>
2018-06-07 16:11:08 +0200

    Add files via upload
    COnstructor getter setters

--- syt4-jpa-westbahn-master/src/main/java/model/Benutzer.java
+++ syt4-jpa-westbahn-master/src/main/java/model/Benutzer.java
@@ -19,4 +19,87 @@ public class Benutzer {
 
 	private Reservierung[] reservierungen;
 
+	public Benutzer(Long ID, String vorName, String nachName, String eMail, String passwort, String smsNummer, Long verbuchtePraemienMeilen, Ticket tickets, Reservierung[] reservierungen) {
+		this.ID = ID;
+		this.vorName = vorName;
+		this.nachName = nachName;
+		this.eMail = eMail;
+		this.passwort = passwort;
+		this.smsNummer = smsNummer;
+		this.verbuchtePraemienMeilen = verbuchtePraemienMeilen;
+		this.tickets = tickets;
+		this.reservierungen = reservierungen;
+	}
+
+	public Long getID() {
+		return ID;
+	}
+
+	public void setID(Long ID) {
+		this.ID = ID;
+	}
+
+	public String getVorName() {
+		return vorName;
+	}
+
+	public void setVorName(String vorName) {
+		this.vorName = vorName;
+	}
+
+	public String getNachName() {
+		return nachName;
+	}
+
+	public void setNachName(String nachName) {
+		this.nachName = nachName;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public String getPasswort() {
+		return passwort;
+	}
+
+	public void setPasswort(String passwort) {
+		this.passwort = passwort;
+	}
+
+	public String getSmsNummer() {
+		return smsNummer;
+	}
+
+	public void setSmsNummer(String smsNummer) {
+		this.smsNummer = smsNummer;
+	}
+
+	public Long getVerbuchtePraemienMeilen() {
+		return verbuchtePraemienMeilen;
+	}
+
+	public void setVerbuchtePraemienMeilen(Long verbuchtePraemienMeilen) {
+		this.verbuchtePraemienMeilen = verbuchtePraemienMeilen;
+	}
+
+	public Ticket getTickets() {
+		return tickets;
+	}
+
+	public void setTickets(Ticket tickets) {
+		this.tickets = tickets;
+	}
+
+	public Reservierung[] getReservierungen() {
+		return reservierungen;
+	}
+
+	public void setReservierungen(Reservierung[] reservierungen) {
+		this.reservierungen = reservierungen;
+	}
 }
