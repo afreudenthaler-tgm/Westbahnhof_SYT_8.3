8c07e5c18472ef5a9481fa17c9921af4c47064c8
afreudenthaler-tgm <afreudenthaler@student.tgm.ac.at>
2018-06-14 01:23:29 +0200

    Sonderangebote persistence and getter/setters

--- syt4-jpa-westbahn-master/src/main/java/model/Sonderangebot.java
+++ syt4-jpa-westbahn-master/src/main/java/model/Sonderangebot.java
@@ -1,8 +1,5 @@
 package model;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -19,7 +16,56 @@ public class Sonderangebot {
 	private int dauer = 12;
 
 	private float preisNachlass = 0.5f;
-
+	@OneToOne
 	private Ticket tickets;
 
+
+	
+	public Long getID() {
+		return ID;
+	}
+
+	public void setID(Long ID) {
+		this.ID = ID;
+	}
+
+	public int getKontingent() {
+		return kontingent;
+	}
+
+	public void setKontingent(int kontingent) {
+		this.kontingent = kontingent;
+	}
+
+	public Date getStartZeit() {
+		return startZeit;
+	}
+
+	public void setStartZeit(Date startZeit) {
+		this.startZeit = startZeit;
+	}
+
+	public int getDauer() {
+		return dauer;
+	}
+
+	public void setDauer(int dauer) {
+		this.dauer = dauer;
+	}
+
+	public float getPreisNachlass() {
+		return preisNachlass;
+	}
+
+	public void setPreisNachlass(float preisNachlass) {
+		this.preisNachlass = preisNachlass;
+	}
+
+	public Ticket getTickets() {
+		return tickets;
+	}
+
+	public void setTickets(Ticket tickets) {
+		this.tickets = tickets;
+	}
 }
